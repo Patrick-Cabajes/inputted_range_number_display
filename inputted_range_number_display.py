911b56e77af0d662e20bf1b6aef9f69ccf1d137f
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-30 23:18:15 +0800

    Done with the 1st step

--- inputted_range_number_display.py
+++ inputted_range_number_display.py
@@ -1,4 +1,10 @@
 #declare each range as an empty array
+var1_10 = []
+var11_20 = []
+var21_30 = []
+var31_40 = []
+var41_50 = []
+
 #build a loop and ask the user to input variables ranging from 1 to 50
     #add conditions to each range
         #append each range to add every new input from the user
