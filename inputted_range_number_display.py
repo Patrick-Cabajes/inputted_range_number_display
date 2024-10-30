64ff880fd022f529fb06e4621eba4a9cbf3def2d
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-30 23:36:53 +0800

    I tested the condition in the first array and it works properly

--- inputted_range_number_display.py
+++ inputted_range_number_display.py
@@ -6,7 +6,14 @@ var31_40 = []
 var41_50 = []
 
 #build a loop and ask the user to input variables ranging from 1 to 50
+while True:
+    value = int(input("Please input numbers from 1 to 50: "))
+    
     #add conditions to each range
+    if value >= 1 and value <= 10:
+        var1_10.append(value)
         #append each range to add every new input from the user
         #print the inputted numbers/variables in each array
         #break
+    else:
+        print("The values you have entered are:", var1_10)
