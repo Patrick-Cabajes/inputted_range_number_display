a2794e910cbacf1dd4cf05cf9a4cdac689e56536
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-30 23:09:21 +0800

    My pseudocode

---
+++ inputted_range_number_display.py
@@ -0,0 +1,6 @@
+#declare each range as an empty array
+#build a loop and ask the user to input variables ranging from 1 to 50
+    #add conditions to each range
+        #append each range to add every new input from the user
+        #print the inputted numbers/variables in each array
+        #break
