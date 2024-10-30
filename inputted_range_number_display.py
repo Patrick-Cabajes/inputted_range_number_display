5b8148f2f9f41a918d9162ba21b5990402b69978
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-31 00:16:06 +0800

    I finally added all the modifications I need and it's now complete

--- inputted_range_number_display.py
+++ inputted_range_number_display.py
@@ -7,30 +7,35 @@ var41_50 = []
 
 #build a loop and ask the user to input variables ranging from 1 to 50
 while True:
-    value = int(input("Please input numbers from 1 to 50: "))
+    try:
+        value = int(input("Please input numbers from 1 to 50: "))
     
     #add conditions to each range
         #append each range to add every new input from the user
-    if value >= 1 and value <= 10:
-        var1_10.append(value)
+        if value >= 1 and value <= 10:
+            var1_10.append(value)
 
-    elif value >= 11 and value <= 20:
-        var11_20.append(value)
-    
-    elif value >= 21 and value <= 30:
-        var21_30.append(value)
+        elif value >= 11 and value <= 20:
+            var11_20.append(value)
+        
+        elif value >= 21 and value <= 30:
+            var21_30.append(value)
 
-    elif value >= 31 and value <= 40:
-        var31_40.append(value)
+        elif value >= 31 and value <= 40:
+            var31_40.append(value)
 
-    elif value >= 41 and value <= 50:
-        var41_50.append(value)
+        elif value >= 41 and value <= 50:
+            var41_50.append(value)
         
         #print the inputted numbers/variables in each array
-    else:
-        print("The values you have entered in the range from 1 to 10 are:", var1_10)
-        print("The values you have entered in the range from 11 to 20 are:", var11_20)
-        print("The values you have entered in the range from 21 to 30 are:", var21_30)
-        print("The values you have entered in the range from 21 to 30 are:", var31_40)
-        print("The values you have entered in the range from 21 to 30 are:", var41_50)
-        break
+        else:
+            print("Invalid Input")
+            print(f"You have inputted {len(var1_10)} number(s) in the range of 1 to 10.")
+            print(f"You have inputted {len(var11_20)} number(s) in the range of 11 to 20.")
+            print(f"You have inputted {len(var21_30)} number(s) in the range of 21 to 30.")
+            print(f"You have inputted {len(var31_40)} number(s) in the range of 31 to 40.")
+            print(f"You have inputted {len(var41_50)} number(s) in the range of 41 to 50.")
+            break
+    
+    except:
+        print("Please input numbers only")
