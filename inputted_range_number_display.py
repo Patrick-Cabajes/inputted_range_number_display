7fc222e95be3f174f8986bd354361c51bdcc7546
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-30 23:44:38 +0800

    The second and third condition also works properly

--- inputted_range_number_display.py
+++ inputted_range_number_display.py
@@ -10,10 +10,19 @@ while True:
     value = int(input("Please input numbers from 1 to 50: "))
     
     #add conditions to each range
+        #append each range to add every new input from the user
     if value >= 1 and value <= 10:
         var1_10.append(value)
-        #append each range to add every new input from the user
+
+    elif value >= 11 and value <= 20:
+        var11_20.append(value)
+    
+    elif value >= 21 and value <= 30:
+        var21_30.append(value)
+        
         #print the inputted numbers/variables in each array
-        #break
     else:
-        print("The values you have entered are:", var1_10)
+        print("The values you have entered in the range from 1 to 10 are:", var1_10)
+        print("The values you have entered in the range from 11 to 20 are:", var11_20)
+        print("The values you have entered in the range from 21 to 30 are:", var21_30)
+        break
